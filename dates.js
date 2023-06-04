e7812266c69f1eff74676d0cbaf42425b78937e5
Jeje <jeremiech06@gmail.com>
2023-06-04 02:21:35 +0200

    modify the index file

---
+++ dates.js
@@ -0,0 +1,2 @@
+document.getElementById('date').innerHTML=new Date(Date.now()).toLocaleString()
+document.getElementById('dates').innerHTML=new Date(Date.now()).getFullYear() +" "+ new Date(Date.now()).getDate()
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './html-pages/index.html'
+  ],
+  theme: {
+    extend: {
+      colors:{
+      'regal-blue':'#0f172a'
+      }
+    },
+  },
+  plugins: [],
+}
+
